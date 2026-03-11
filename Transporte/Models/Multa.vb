2d194c65734cd97ef3178902a89bb5f120ceb811
Andrei Mesén Romero <mrandrei.dev@gmail.com>
2026-03-10 20:44:30 -0600

    Creación de los Models para TipoMulta y Multa

--- Transporte/Models/Multa.vb
+++ Transporte/Models/Multa.vb
@@ -1,3 +1,76 @@
-﻿Public Class Multa
+﻿Namespace Models
+    Public Class Multa
+        Private _idMulta As Integer
+        Private _idVehiculo As Integer
+        Private _idTipoMulta As Integer
+        Private _fecha As DateTime
+        Private _montoAplicado As Decimal
+        Private _pagada As Boolean
 
-End Class
+        Public Sub New()
+        End Sub
+
+        Public Sub New(idMulta As Integer, idVehiculo As Integer, idTipoMulta As Integer, fecha As Date, montoAplicado As Decimal, pagada As Boolean)
+            _idMulta = idMulta
+            _idVehiculo = idVehiculo
+            _idTipoMulta = idTipoMulta
+            _fecha = fecha
+            _montoAplicado = montoAplicado
+            _pagada = pagada
+        End Sub
+
+        Public Property IdMulta As Integer
+            Get
+                Return _idMulta
+            End Get
+            Set(value As Integer)
+                _idMulta = value
+            End Set
+        End Property
+
+        Public Property IdVehiculo As Integer
+            Get
+                Return _idVehiculo
+            End Get
+            Set(value As Integer)
+                _idVehiculo = value
+            End Set
+        End Property
+
+        Public Property IdTipoMulta As Integer
+            Get
+                Return _idTipoMulta
+            End Get
+            Set(value As Integer)
+                _idTipoMulta = value
+            End Set
+        End Property
+
+        Public Property Fecha As Date
+            Get
+                Return _fecha
+            End Get
+            Set(value As Date)
+                _fecha = value
+            End Set
+        End Property
+
+        Public Property MontoAplicado As Decimal
+            Get
+                Return _montoAplicado
+            End Get
+            Set(value As Decimal)
+                _montoAplicado = value
+            End Set
+        End Property
+
+        Public Property Pagada As Boolean
+            Get
+                Return _pagada
+            End Get
+            Set(value As Boolean)
+                _pagada = value
+            End Set
+        End Property
+    End Class
+End Namespace
---
+++ Transporte/Models/TipoMulta.vb
@@ -0,0 +1,54 @@
+﻿Namespace Models
+    Public Class TipoMulta
+        Private _idTipoMulta As Integer
+        Private _descripcion As String
+        Private _montoBase As Decimal
+        Private _activa As Boolean
+
+        Public Sub New()
+        End Sub
+
+        Public Sub New(idTipoMulta As Integer, descripcion As String, montoBase As Decimal, activa As Boolean)
+            _idTipoMulta = idTipoMulta
+            _descripcion = descripcion
+            _montoBase = montoBase
+            _activa = activa
+        End Sub
+
+        Public Property IdTipoMulta As Integer
+            Get
+                Return _idTipoMulta
+            End Get
+            Set(value As Integer)
+                _idTipoMulta = value
+            End Set
+        End Property
+
+        Public Property Descripcion As String
+            Get
+                Return _descripcion
+            End Get
+            Set(value As String)
+                _descripcion = value
+            End Set
+        End Property
+
+        Public Property MontoBase As Decimal
+            Get
+                Return _montoBase
+            End Get
+            Set(value As Decimal)
+                _montoBase = value
+            End Set
+        End Property
+
+        Public Property Activa As Boolean
+            Get
+                Return _activa
+            End Get
+            Set(value As Boolean)
+                _activa = value
+            End Set
+        End Property
+    End Class
+End Namespace
