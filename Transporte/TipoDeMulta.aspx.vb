f9116f3c7c699280dfcb04eee548f8fe28dfe003
lTamalinl <johansj56@gmail.com>
2026-03-17 20:55:19 -0600

    Se coloca el monto por tipo de multa

--- Transporte/TipoDeMulta.aspx.vb
+++ Transporte/TipoDeMulta.aspx.vb
@@ -5,4 +5,45 @@
 
     End Sub
 
+    Protected Sub ddlDescripcion_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+        Dim monto As Integer = 0
+        Dim opcion As Integer = Convert.ToInt32(ddlDescripcion.SelectedValue)
+
+        Select Case ddlDescripcion.SelectedValue.Trim()
+
+            Case "1"
+                txtMonto.Text = "200,195.06 colones"
+
+            Case "2"
+                txtMonto.Text = "122,597.53 colones"
+
+            Case "3"
+                txtMonto.Text = "122,597.58 colones"
+
+            Case "4"
+                txtMonto.Text = "362,839.56 colones"
+
+
+            Case "5"
+                txtMonto.Text = "122,800.96 colones"
+
+            Case "6"
+                txtMonto.Text = "245,195.06 colones"
+
+            Case "7"
+                txtMonto.Text = "130,525.11 colones"
+
+            Case "8"
+                txtMonto.Text = "80,000 colones"
+
+            Case Else
+                txtMonto.Text = ""
+
+        End Select
+
+
+
+    End Sub
+
 End Class
