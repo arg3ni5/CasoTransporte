2d94b5de4eed9fc868a14236103375f356a9de76
rgmora99 <rgmora@correos.go.cr>
2026-03-24 19:12:57 -0600

    # cambios

---
+++ Transporte/Models/Propietario.vb
@@ -0,0 +1,64 @@
+﻿Namespace Models
+    Public Class Propietario
+        Private _idPersona As Integer
+        Private _telefono As String
+        Private _direccion As String
+        Private _nombreCompleto As String
+
+        Public Sub New()
+        End Sub
+
+        Public Sub New(idPersona As Integer, telefono As String, direccion As String)
+            Me.IdPersona = idPersona
+            Me.Telefono = telefono
+            Me.Direccion = direccion
+        End Sub
+
+        Public Sub New(idPersona As Integer, nombreCompleto As String, telefono As String, direccion As String)
+            Me.IdPersona = idPersona
+            Me.NombreCompleto = nombreCompleto
+            Me.Telefono = telefono
+            Me.Direccion = direccion
+        End Sub
+
+        Public Property IdPersona As Integer
+            Get
+                Return _idPersona
+            End Get
+            Set(value As Integer)
+                _idPersona = value
+            End Set
+        End Property
+
+        Public Property Telefono As String
+            Get
+                Return _telefono
+            End Get
+            Set(value As String)
+                _telefono = value
+            End Set
+        End Property
+
+        Public Property Direccion As String
+            Get
+                Return _direccion
+            End Get
+            Set(value As String)
+                _direccion = value
+            End Set
+        End Property
+
+        Public Property NombreCompleto As String
+            Get
+                Return _nombreCompleto
+            End Get
+            Set(value As String)
+                _nombreCompleto = value
+            End Set
+        End Property
+
+        Public Function Resumen() As String
+            Return $"IdPersona: {IdPersona} - Nombre: {NombreCompleto} - Teléfono: {Telefono} - Dirección: {Direccion}"
+        End Function
+    End Class
+End Namespace
---
+++ Transporte/utils/DbHelper.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Data.SqlClient
+Imports System.Configuration
+
+Namespace Utils
+    Public Class DbHelper
+
+        Private connectionString As String = ConfigurationManager.ConnectionStrings("II-46ConnectionString").ConnectionString
+
+        Public Function GetConnection() As SqlConnection
+            Dim conn As New SqlConnection(connectionString)
+
+            Try
+                conn.Open()
+            Catch ex As Exception
+                conn.Dispose()
+                Throw New Exception("Error al abrir la conexión: " & ex.Message)
+            End Try
+
+            Return conn
+        End Function
+
+        Public Function ExecuteNonQuery(query As String, parameters As Dictionary(Of String, Object), ByRef errorMessage As String) As Boolean
+            Try
+                Using conn As SqlConnection = GetConnection()
+                    Using cmd As New SqlCommand(query, conn)
+
+                        If parameters IsNot Nothing Then
+                            For Each p In parameters
+                                cmd.Parameters.AddWithValue(p.Key, p.Value)
+                            Next
+                        End If
+
+                        cmd.ExecuteNonQuery()
+                        Return True
+                    End Using
+                End Using
+            Catch ex As Exception
+                errorMessage = "Error al ejecutar la consulta: " & ex.Message
+                Return False
+            End Try
+        End Function
+
+    End Class
+End Namespace
